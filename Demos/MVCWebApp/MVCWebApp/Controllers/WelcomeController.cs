05446de5bdc11443f471da9c7dfc4bb1e6346a12
Sharad Singh <sharadksingh67@gmail.com>
2026-01-09 12:42:09 +0530

    9Jan

---
+++ Demos/MVCWebApp/MVCWebApp/Controllers/WelcomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCWebApp.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Greet()
+        {
+            return View();
+        }
+    }
+}
