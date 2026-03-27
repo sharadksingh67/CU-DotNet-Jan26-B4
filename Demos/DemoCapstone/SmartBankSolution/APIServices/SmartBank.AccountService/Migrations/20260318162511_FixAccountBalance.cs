5ff8c0f41432e692da8d57aa8e141cb4e200c49b
Sharad Singh <sharadksingh67@gmail.com>
2026-03-27 11:54:45 +0530

    week 12 demo,assignment added

---
+++ Demos/DemoCapstone/SmartBankSolution/APIServices/SmartBank.AccountService/Migrations/20260318162511_FixAccountBalance.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartBank.AccountService.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAccountBalance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Demos/DemoCapstone/SmartBankSolution/APIServices/SmartBank.AuthService/Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SmartBank.AuthService.DTOs;
+using SmartBank.AuthService.Models;
+
+namespace SmartBank.AuthService.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly TokenService _tokenService;
+
+        public AuthController(ILogger<AuthController> logger,
+            UserManager<ApplicationUser> userManager,
+                              TokenService tokenService)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = dto.Email,
+                Email = dto.Email,
+                FullName = dto.FullName
+            };
+
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+            {
+                _logger.LogError("User could not register");
+                return BadRequest(result.Errors);
+            }
+            _logger.LogInformation("User Registered...");
+            // Assign role
+            await _userManager.AddToRoleAsync(user, dto.Role);
+
+            return Ok("User registered successfully");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var user = await _userManager.FindByEmailAsync(dto.Email);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, dto.Password))
+                return Unauthorized("Invalid credentials");
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var token = _tokenService.CreateToken(user, roles);
+
+            return Ok(new { token });
+        }
+    }
+}
